b34a9b7aab7364b92cceb1e7ca52e3349374ea65
choudharyRahul22 <32006288+choudharyRahul22@users.noreply.github.com>
2017-11-19 01:55:55 +0530

    Angular

--- Projects/Tracker/src/app/tracker/tracker.component.ts
+++ Projects/Tracker/src/app/tracker/tracker.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-tracker',
@@ -22,10 +22,85 @@ export class TrackerComponent implements OnInit {
   contextSwitch = ['Yes' , 'No'];
   dublicate = ['Yes' , 'No'];
 
-  recipeForm: FormGroup;
+  trackerForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
   }
+
+  onSubmit() {
+    console.log(this.trackerForm);
+  }
+
+  private initForm() {
+    let monthName = '';
+    let jiraId = '';
+    let customerName = '';
+    let jiraDescription = '';
+    let developerName = '';
+    let priority = '';
+    let severity = '';
+    let version = '';
+    let etaToFix = '';
+    let defectType = '';
+    let startDate = '';
+    let endDate = '';
+    let status = '';
+    let stepToReproduce = '';
+    let stepToReproduceDetailsIfAny = '';
+    let followUpWithGS2 = '';
+    let environmentSetup = '';
+    let analysis = '';
+    let devlopement = '';
+    let codeReview = '';
+    let providedSupportToQA = '';
+    let rework = '';
+    let portingRequired = '';
+    let portingJiraId = '';
+    let portingEffort = '';
+    let reason = '';
+    let totalEffort = '';
+    let complexity = '';
+    let contextSwitch = '';
+    let monthCode = '';
+    let dublicate = '';
+
+
+    this.trackerForm = new FormGroup({
+      'monthName': new FormControl(monthName, Validators.required),
+      'jiraId': new FormControl(jiraId, Validators.required),
+      'customerName': new FormControl(customerName, Validators.required),
+      'jiraDescription': new FormControl(jiraDescription, Validators.required),
+      'developerName': new FormControl(developerName, Validators.required),
+      'priority': new FormControl(priority, Validators.required),
+      'severity': new FormControl(severity, Validators.required),
+      'version': new FormControl(version, Validators.required),
+      'etaToFix': new FormControl(etaToFix, Validators.required),
+      'defectType': new FormControl(defectType, Validators.required),
+      'startDate': new FormControl(startDate, Validators.required),
+      'endDate': new FormControl(endDate, Validators.required),
+      'status': new FormControl(status, Validators.required),
+      'stepToReproduce': new FormControl(stepToReproduce, Validators.required),
+      'stepToReproduceDetailsIfAny': new FormControl(stepToReproduceDetailsIfAny, Validators.required),
+      'followUpWithGS2': new FormControl(followUpWithGS2, Validators.required),
+      'environmentSetup': new FormControl(environmentSetup, Validators.required),
+      'analysis': new FormControl(analysis, Validators.required),
+      'devlopement': new FormControl(devlopement, Validators.required),
+      'codeReview': new FormControl(codeReview, Validators.required),
+      'providedSupportToQA': new FormControl(providedSupportToQA, Validators.required),
+      'rework': new FormControl(rework, Validators.required),
+      'portingRequired': new FormControl(portingRequired, Validators.required),
+      'portingJiraId': new FormControl(portingJiraId, Validators.required),
+      'portingEffort': new FormControl(portingEffort, Validators.required),
+      'reason': new FormControl(reason, Validators.required),
+      'totalEffort': new FormControl(totalEffort, Validators.required),
+      'complexity': new FormControl(complexity, Validators.required),
+      'contextSwitch': new FormControl(contextSwitch, Validators.required),
+      'monthCode': new FormControl(monthCode, Validators.required),
+      'dublicate': new FormControl(dublicate, Validators.required),
+    });
+  }
+
 
 }
